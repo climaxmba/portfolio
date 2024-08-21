7bca115d9866feaaa1e88d49478233ac88bd93bb
Climax Mba <climaxium150@gmail.com>
2024-08-21 04:50:41 +0100

    Add about section contents

--- src/app/components/about/About..tsx
+++ src/app/components/about/About..tsx
@@ -2,5 +2,22 @@
 import styles from "./about.module.scss";
 
 export default function About() {
-  return <section>About</section>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.about}>
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+          tempora illo repudiandae? Similique saepe quo quae iusto esse officia
+          quod quidem inventore, aperiam aliquam non praesentium laudantium,
+          perspiciatis labore reprehenderit omnis tenetur consequatur facilis
+          vitae molestias a quasi veritatis. Natus!
+        </p>
+        <button type="button">CTA</button>
+      </div>
+      <div className={styles.imageContainer}>
+        <span>IMG</span>
+      </div>
+    </section>
+  );
 }
