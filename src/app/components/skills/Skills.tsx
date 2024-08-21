a16fdd5348389a80f5bd62a1323de2a639c88094
Climax Mba <climaxium150@gmail.com>
2024-08-21 09:47:36 +0100

    Add skills section data

--- src/app/components/skills/Skills.tsx
+++ src/app/components/skills/Skills.tsx
@@ -1,6 +1,20 @@
 "use client";
+
+import skills from "@/assets/skillsData";
 import styles from "./skills.module.scss";
 
 export default function Skills() {
-  return <section>Skills</section>;
+  return (
+    <section className={styles.container}>
+      <h2>What I work with</h2>
+      <ul className={styles.skillList}>
+        {skills.map((skill) => (
+          <li key={skill.text}>
+            <skill.Icon />
+            <span>{skill.text}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
