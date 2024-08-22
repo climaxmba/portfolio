9ad8f9555a7d6b09dffc03aa86c42eefde8e036c
Climax Mba <climaxium150@gmail.com>
2024-08-22 19:39:59 +0100

    Add footer component & fix `main` width

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.container}>
+      <h1>Climax Mba - Frontend Developer</h1>
+      <div className={styles.socialIcons}>LinkedIn</div>
+    </footer>
+  );
+}
