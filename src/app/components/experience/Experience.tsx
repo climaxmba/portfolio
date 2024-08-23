ed411dcd9e2b12c861b1d16fc9cb068b9b4bc703
Climax Mba <climaxium150@gmail.com>
2024-08-23 09:09:28 +0100

    Add timeline effects

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -1,34 +1,38 @@
 "use client";
 
 import experiences from "@/app/assets/data/experienceData";
+import Image from "next/image";
 
 import styles from "./experience.module.scss";
-import Image from "next/image";
 
 export default function Experience() {
   return (
     <section className={styles.container}>
       <h2>Experience</h2>
-      <ul>
-        {experiences.map((experience, i) => (
-          <li key={i}>
-            <h3>{experience.role}</h3>
-            <a href={experience.company.url} className={styles.company}>
-              <Image
-                alt="Company's Logo"
-                src={experience.company.image.src}
-                height={30}
-                width={30}
-              />
-              {experience.company.name}
-            </a>
-            <p className={styles.periodLocation}>
-              {experience.period} | {experience.location}
-            </p>
-            <p className={styles.description}>{experience.description}</p>
-          </li>
-        ))}
-      </ul>
+
+      <div className={styles.timeline}>
+        <div className={styles.ruler}></div>
+        <ul>
+          {experiences.map((experience, i) => (
+            <li key={i}>
+              <h3>{experience.role}</h3>
+              <a href={experience.company.url} className={styles.company}>
+                <Image
+                  alt="Company's Logo"
+                  src={experience.company.image.src}
+                  height={38}
+                  width={38}
+                />
+                {experience.company.name}
+              </a>
+              <p className={styles.periodLocation}>
+                {experience.period} | {experience.location}
+              </p>
+              <p className={styles.description}>{experience.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
