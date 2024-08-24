a5d1ecaee84b15dfe4c37f8ef3653b92e7893268
Climax Mba <climaxium150@gmail.com>
2024-08-24 03:57:23 +0100

    Modify about section header

--- src/app/components/about/About..tsx
+++ src/app/components/about/About..tsx
@@ -6,7 +6,10 @@ export default function About() {
   return (
     <section className={styles.container}>
       <div className={styles.about}>
-        <h2>I am Climax Mba, Frontend Developer</h2>
+        <h1 className={styles.header}>
+          I am <span className={styles.name}>Climax Mba</span>,{" "}
+          <span className={styles.role}>Frontend Developer</span>.
+        </h1>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
           tempora illo repudiandae? Similique saepe quo quae iusto esse officia
@@ -14,7 +17,9 @@ export default function About() {
           perspiciatis labore reprehenderit omnis tenetur consequatur facilis
           vitae molestias a quasi veritatis. Natus!
         </p>
-        <button type="button" className="icon-btn">Contact Me <Plane /></button>
+        <button type="button" className="icon-btn">
+          Contact Me <Plane />
+        </button>
       </div>
       <div className={styles.imageContainer}>
         <span>IMG</span>
