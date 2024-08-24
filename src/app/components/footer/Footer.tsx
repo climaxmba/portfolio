734b3fa13e0bf22918db088c7424cddb8dc69232
Climax Mba <climaxium150@gmail.com>
2024-08-24 03:16:30 +0100

    Modify footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,10 +1,22 @@
+import { GitHub, LinkedIn } from "../icons/Icons";
 import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.container}>
-      <h1>Climax Mba - Frontend Developer</h1>
-      <div className={styles.socialIcons}>LinkedIn</div>
+      <h1>Copywrite &copy; {new Date().getFullYear()}; Climax Mba</h1>
+      <div className={styles.socialIcons}>
+        <a
+          href="https://www.linkedin.com/in/climaxmba/"
+          target="_blank"
+          title="LinkedIn"
+        >
+          <LinkedIn />
+        </a>
+        <a href="https://github.com/climaxmba" target="_blank" title="GitHub">
+          <GitHub />
+        </a>
+      </div>
     </footer>
   );
 }
