dce379e1f4f9bf06963df069ecfe3154c83adbec
Climax Mba <climaxium150@gmail.com>
2024-08-26 20:31:26 +0100

    Replace logo

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <footer className={styles.container}>
-      <Image alt="Logo" src="/logo.webp" height={64} width={64} />
+      <Image alt="Logo" src="/logo.svg" height={64} width={64} />
       <h1>Copywrite &copy; {new Date().getFullYear()}; Climax Mba</h1>
       <div className={styles.socialIcons}>
         <a
